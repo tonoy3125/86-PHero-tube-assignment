8fcc587b1376bea434100ebe1fcc0033af8dea5f
tonoy3125 <shaifshajedt@gmail.com>
2023-09-01 01:41:46 +0600

    card title commit

--- index.js
+++ index.js
@@ -22,26 +22,28 @@ const handleLoadCategories = async (categoryId) => {
     const getdata = data.data;
 
     const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
     getdata.forEach(data => {
         console.log(data)
         const div = document.createElement('div');
         div.innerHTML = `
         <div>
-        <div class="card image-full w-[312px] h-[200px] mx-auto">
+            <div class="card image-full w-[312px] h-[200px] mx-auto">
             <figure><img src="${data?.thumbnail}" alt="Shoes" /></figure>
-        </div>
-        <div class="flex">
-            <div>
-                <div class="avatar">
-                    <div class="rounded-full w-[40px] h-[40px]">
+            </div>
+            <div class="flex gap-3 ml-8 mt-5">
+                <div>
+                    <div class="avatar">
+                        <div class="rounded-full w-[40px] h-[40px]">
                         <img src="${data?.authors[0].profile_picture}" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <h2 class="card-title">${data?.title}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-            </div>
+                <div>
+                <h2 class="text-[#171717]">${data?.title}</h2>
+                <p>${data?.authors[0].profile_name}</p>
+                <p>${data?.others?.views}</p>
+                </div>
         </div>
     </div>
         `;
@@ -53,3 +55,5 @@ const handleLoadCategories = async (categoryId) => {
 
 
 getCategories()
+
+handleLoadCategories('1000')
