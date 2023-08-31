c672c3c52a2bf1d9c460fff66cb2c54c8315f131
tonoy3125 <shaifshajedt@gmail.com>
2023-09-01 00:58:14 +0600

    categories button commit

--- index.js
+++ index.js
@@ -16,7 +16,38 @@ const getCategories = async () => {
 
 }
 
+const handleLoadCategories = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+    const data = await response.json()
+    const getdata = data.data;
 
+    const cardContainer = document.getElementById('card-container');
+    getdata.forEach(data => {
+        console.log(data)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div>
+        <div class="card image-full w-[312px] h-[200px] mx-auto">
+            <figure><img src="${data?.thumbnail}" alt="Shoes" /></figure>
+        </div>
+        <div class="flex">
+            <div>
+                <div class="avatar">
+                    <div class="rounded-full w-[40px] h-[40px]">
+                        <img src="${data?.authors[0].profile_picture}" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h2 class="card-title">${data?.title}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+        </div>
+    </div>
+        `;
+        cardContainer.appendChild(div)
+    });
+}
 
 
 
